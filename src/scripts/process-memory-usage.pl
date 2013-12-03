b4e5d2ea5f889977d0991263036e7485e8638828
Darlene Stewart <Darlene.Stewart@nrc-cnrc.gc.ca>
2013-12-03 10:51:34 -0500

    Report USS (from smem) in process-memory-usage.pl.

--- src/scripts/process-memory-usage.pl
+++ src/scripts/process-memory-usage.pl
@@ -68,6 +68,7 @@ local $| = 1; # autoflush
 
 my $i = 0;
 my $not_present = 0;
+my $use_smem = system("which-test.sh smem") == 0;
 while (1) {
    my %PIDS = ();
    my $total_vsz = 0;
@@ -92,6 +93,30 @@ while (1) {
          $total_pcpu += $pcpu;
       }
    }
+   my $total_uss_smem = 0;
+   my $total_rss_smem = 0;
+   my $total_vss_smem = 0;
+   my $total_swap_smem = 0;
+   if ($use_smem) {
+      foreach my $process_info (split /\n/, `smem -c "pid uss rss vss swap name" 2> /dev/null`) {
+         print "<V> $process_info\n" if ($verbose > 1);
+   
+         next if ($process_info =~ /PID/);  # Skip the header
+   
+         chomp($process_info);        # Remove newline
+         $process_info =~ s/^\s+//;   # Remove leading spaces
+         my ($pid, $uss, $rss, $vss, $swap, $name) = split(/\s+/, $process_info);
+   
+         # Is this process part of the process tree
+         if (exists $PIDS{$pid}) {
+            print "<D> $pid $uss $rss $vss $swap $name\n" if(defined($debug));
+            $total_uss_smem += $uss;
+            $total_rss_smem += $rss;
+            $total_vss_smem += $vss;
+            $total_swap_smem += $swap;
+         }
+      }
+   }
    printf("<D> num sub p: %d\n", scalar(keys %PIDS)) if (defined($debug));
    if ($stop_after and scalar(keys %PIDS) == 0) {
       ++$not_present;
@@ -103,6 +128,17 @@ while (1) {
    $total_vsz = $total_vsz / $Gigabytes;
    $total_rss = $total_rss / $Gigabytes;
    printf "$date Total vsz: %.3fG rss: %.3fG pcpu: %.1f%% ", $total_vsz , $total_rss, $total_pcpu;
+   if ($use_smem) {
+      $total_uss_smem = $total_uss_smem / $Gigabytes;
+      $total_rss_smem = $total_rss_smem / $Gigabytes;
+      $total_vss_smem = $total_vss_smem / $Gigabytes;
+      $total_swap_smem = $total_swap_smem / $Gigabytes;
+      printf "uss(smem): %.3fG ", $total_uss_smem;
+      if ($verbose > 1) {
+         printf "rss(smem): %.3fG vss(smem): %.3fG swap(smem): %.3fG ",
+                $total_rss_smem, $total_vss_smem, $total_swap_smem;
+      }
+   }
    printf "%d Processes\n", scalar (keys (%PIDS));
 
    sleep $sleep_time;
