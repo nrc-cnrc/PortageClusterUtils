ad7d4d7e00fa698dc92324295f6f605ed78eabfa
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2015-06-10 09:11:44 -0400

    run-parallel.sh: unset the trap earlier.
    This patch is probably not necessary, but I ran into a case where I had an
    infinite loop, having run-parallel.sh re-execute the trap 0 code at full speed
    for a few weeks until I noticed it. The safeguard I'm committing here should
    not add anything, in theory, but it makes me feel more confident that such
    freak cases won't happen again. (I can't reproduce the problem, either with
    run-parallel.sh or at a smaller scale.)

--- src/scripts/run-parallel.sh
+++ src/scripts/run-parallel.sh
@@ -408,6 +408,7 @@ GLOBAL_RETURN_CODE=2
 # Process clean up at exit or kill - set this trap early enough that we
 # clean up no matter what happens.
 trap '
+   trap "" 0 1 13 14
    if [[ -n "$WORKER_JOBIDS" ]]; then
       WORKERS=`cat $WORKER_JOBIDS`
       qdel $WORKERS >& /dev/null
@@ -438,7 +439,6 @@ trap '
    fi
    test -n "$DEBUG" || rm -rf ${LOGFILEPREFIX}log.worker* $WORKDIR
    [[ -f $LOGFILEPREFIX ]] && rm -f $LOGFILEPREFIX
-   trap "" 0
    exit $GLOBAL_RETURN_CODE
 ' 0 1 13 14
 
