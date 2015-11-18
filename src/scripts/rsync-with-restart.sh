fc2588e070bc352b610a118a47ad23f2f33a5a61
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2015-11-18 15:44:25 -0500

    rsync-with-restart.sh: by dftl, abort if dest exists, since would overwrite

--- src/scripts/rsync-with-restart.sh
+++ src/scripts/rsync-with-restart.sh
@@ -3,18 +3,19 @@
 # @file rsync-with-restart.sh
 # @brief Self-resuming and recovering copy that tried until the copy is really done.
 # @author Eric Joanis
-# 
+#
 # Traitement multilingue de textes / Multilingual Text Processing
 # Tech. de l'information et des communications / Information and Communications Tech.
 # Conseil national de recherches Canada / National Research Council Canada
 # Copyright 2015, Sa Majeste la Reine du Chef du Canada /
 # Copyright 2015, Her Majesty in Right of Canada
 
+[[ $0 =~ [^/]*$ ]] && PROG=$BASH_REMATCH || PROG=$0
+
 usage() {
    for msg in "$@"; do
       echo $msg >&2
    done
-   [[ $0 =~ [^/]*$ ]] && PROG=$BASH_REMATCH || PROG=$0
    cat <<==EOF== >&2
 
 Usage: $PROG SOURCE DESTINATION
@@ -33,6 +34,13 @@ Recommended usage:
   (path or permission errors, e.g.), it will keep trying every 30 seconds
   anyway. Inspect your job logs or the destination folder to make sure the copy
   is really starting.  (qpeek -e JOBID can help.)
+
+  Warning: the destination directory should not exist, because files found
+  there will be deleted if they don't also exist in the source directory.
+
+Options:
+  -h(elp)    Print this help message
+  -f(orce)   Run this script even if the destination directory exists
 ==EOF==
 
    exit 1
@@ -40,16 +48,48 @@ Recommended usage:
 
 while [[ $# -gt 0 ]]; do
    case "$1" in
+   -f|--force) FORCE=1;;
    -*) usage;;
    *) break;;
    esac
    shift
 done
 
-[[ $# -eq 2 ]] || usage "Error: This script takes exactly two arguments."
+[[ $# -eq 2 ]] || usage "ERROR: this script takes exactly two arguments."
+
+shopt -s compat31
+
+DEST_EXISTS=
+if [[ $2 =~ "(..*):(..*)" ]]; then
+   DEST_HOST=${BASH_REMATCH[1]}
+   DEST_PATH=${BASH_REMATCH[2]}
+   if ! ssh $DEST_HOST true; then
+      echo "$PROG ERROR: cannot connect to destination host $DEST_HOST. Aborting."
+      exit 1
+   fi
+   if ssh "$DEST_HOST" test -e "\"$DEST_PATH\""; then
+      DEST_EXISTS=1
+   fi
+else
+   if [[ -e $2 ]]; then
+      DEST_EXISTS=1
+   fi
+fi >&2
+
+if [[ $DEST_EXISTS ]]; then
+   if [[ $FORCE ]]; then
+      echo "$PROG Warning: Destination directory $2 exists."
+      echo "Proceeding anyway because --force was specified."
+      echo "Files in the destination will be deleted if they don't exist in the source."
+   else
+      echo "$PROG ERROR: destination directory $2 exists. Aborting."
+      exit 1
+   fi
+fi >&2
+
 
 iter=0
-while true; do 
+while true; do
    iter=$((iter+1))
    echo ITER $iter
    echo rsync -arz --partial --timeout=30 --delete --stats "$@"
