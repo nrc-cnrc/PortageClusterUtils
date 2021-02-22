412da9c853670767bfb5bcaed56d4aaa2149db36
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2021-02-22 11:28:27 -0500

    Add a SETUP.bash script to activate cluster utils in place in lieu of installing them

---
+++ SETUP.bash
@@ -0,0 +1,24 @@
+# @file SETUP.bash
+# @brief Source this file to add the Cluster Utils tools to your PATH in place.
+#
+# This configuration script is a convenient alternative that can be used to puth
+# the scripts on your PATH in place instead of running "make install" inside
+# src/scripts
+#
+# @author Eric Joanis
+#
+# Traitement multilingue de textes / Multilingual Text Processing
+# Technologies numériques / Digital Technologies
+# Conseil national de recherches Canada / National Research Council Canada
+# Copyright 2021, Sa Majeste la Reine du Chef du Canada /
+# Copyright 2021, Her Majesty in Right of Canada
+
+echo "ClusterUtils, NRC-CNRC, (c) 2005 - 2021, Her Majesty in Right of Canada" >&2
+
+SOURCE="${BASH_SOURCE[0]}"
+if [[ -h $SOURCE ]]; then
+    SOURCE=$(readlink -f $SOURCE)
+fi
+BASE_DIR="$( cd "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+echo "ClusterUtils path: $BASE_DIR" >&2
+export PATH=$BASE_DIR/src/scripts:$PATH
