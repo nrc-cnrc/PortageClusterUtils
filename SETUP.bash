734bebc88665ddde88f797515324dd87bb40cec5
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2021-02-26 08:59:15 -0500

    The name of this repo is now PortageClusterUtils

--- SETUP.bash
+++ SETUP.bash
@@ -1,5 +1,5 @@
 # @file SETUP.bash
-# @brief Source this file to add the Cluster Utils tools to your PATH in place.
+# @brief Source this file to add the Portage Cluster Utils tools to your PATH in place.
 #
 # This configuration script is a convenient alternative that can be used to puth
 # the scripts on your PATH in place instead of running "make install" inside
@@ -13,12 +13,12 @@
 # Copyright 2021, Sa Majeste la Reine du Chef du Canada /
 # Copyright 2021, Her Majesty in Right of Canada
 
-echo "ClusterUtils, NRC-CNRC, (c) 2005 - 2021, Her Majesty in Right of Canada" >&2
+echo "PortageClusterUtils, NRC-CNRC, (c) 2005 - 2021, Her Majesty in Right of Canada" >&2
 
 SOURCE="${BASH_SOURCE[0]}"
 if [[ -h $SOURCE ]]; then
     SOURCE=$(readlink -f $SOURCE)
 fi
 BASE_DIR="$( cd "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-echo "ClusterUtils path: $BASE_DIR" >&2
+echo "PortageClusterUtils path: $BASE_DIR" >&2
 export PATH=$BASE_DIR/src/scripts:$PATH
