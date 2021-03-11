3c1bf56dc7946dfa9a853abc1527bd03daa21071
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2021-03-11 11:31:06 -0500

    bug fixes: psub -c option and r-parallel-worker.pl $me var
    psub -c: the option should be set before its value is read...
    
    r-parallel-worker.pl $me var: get $JOB_ID with the current correct way to do so.

--- bin/r-parallel-worker.pl
+++ bin/r-parallel-worker.pl
@@ -62,7 +62,7 @@ if($help ne ''){
 my $me = `uname -n`;
 chomp $me;
 $me =~ s/(.)\..*/$1/; # Remove domain qualifier - unqualified host name is enough info
-$me .= ":" . ($ENV{PBS_JOBID} || $ENV{GECOSHEP_JOB_ID} || "");
+$me .= ":" . ($ENV{JOB_ID} || "");
 my $need_sleep = ($me =~ /balzac/);
 $me =~ s/(\d)\..*/$1/; # Remove domain qualifier from job number too, superfluous
 
